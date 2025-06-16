515d89d749a0b6bb179d8e4deebba6a2b0ae4a6d
Renato Moreira Beltrão Filho <161071024+RenatoBeltrao@users.noreply.github.com>
2025-06-16 14:37:48 -0300

    atualizanso o main
    refazendo o testo dos prints de envio padrao e econômico

--- src/main/java/padroes/projeto/strategy/Main.java
+++ src/main/java/padroes/projeto/strategy/Main.java
@@ -13,11 +13,11 @@ public class Main {
         // Configure a estratégia de cálculo de custo de envio como "envio padrao"
         calculadora.setStrategy(new EnvioPadrao());
         double custoEnvioPadrao = calculadora.calcularCustoEnvio(5.0);
-        System.out.println("Custo de envio expresso: $" + custoEnvioPadrao);
+        System.out.println("Custo de envio padrão: $" + custoEnvioPadrao);
 
         // Configure a estratégia de cálculo de custo de envio como "envio economico"
         calculadora.setStrategy(new EnvioEconomico());
         double custoEnvioEconomico = calculadora.calcularCustoEnvio(5.0);
-        System.out.println("Custo de envio expresso: $" + custoEnvioEconomico);
+        System.out.println("Custo de envio econômico: $" + custoEnvioEconomico);
     }
 }
